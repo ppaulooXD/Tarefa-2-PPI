c7026eb82ab4bc102a1cdf1796d20d7c8a1a07d9
Paulo Henrique Amorim Dultra <ph.total12@gmail.com>
2023-11-12 11:32:15 -0300

    feats: exibição de lista de usuarios cadastrados

--- index.js
+++ index.js
@@ -3,6 +3,60 @@ import express from 'express';
 const porta = 3000;
 const host = '0.0.0.0';
 
+var usuarios= [];
+
+function listausuarios(requisicao, resposta){
+    const usuario = {
+        nome: requisicao.query.nome,
+        sobrenome: requisicao.query.sobrenome,
+        username: requisicao.query.username
+    }
+    usuarios.push(usuario);
+
+    let contresposta =`
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Lista de usuários</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    
+                </head>
+                <body>
+                <h1>Lista de usuários</h1>
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Sobrenome</th>
+                        <th scope="col">Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>                      
+    `;
+    for(const usuario of usuarios)
+    {
+        contresposta += `
+            <tr>    
+                <td>${usuario.nome}</td>
+                <td>${usuario.sobrenome}</td>
+                <td>${usuario.username}</td>
+            </tr>
+        `
+    }
+    contresposta += `
+                    </tbody>
+                </table>
+                <a class="btn btn-light" href="/" role="button">Voltar ao menu</a>
+                <a class="btn btn-success" href="/cadastro.html" role="button">Cadastrar</a>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                </body>
+                </html>`;
+
+    resposta.send(contresposta);
+                
+}
+
 const app = express();
 
 app.use(express.static('./páginas'));
@@ -35,7 +89,7 @@ app.get('/', (requisicao, resposta) => {
                         }
                         .a {
                             text-decoration: none;
-                            background-color: blue;
+                            background-color: black;
                             color: white;
                             border-radius: 5px;
                             padding: 10px;
@@ -51,6 +105,9 @@ app.get('/', (requisicao, resposta) => {
                 </html>       
     `);
 })
+
+
+app.get('/cadastro', listausuarios);
 
 app.listen(porta, host, () => {
     console.log(`Servidor rodando na url http://${host}:${porta}`);
